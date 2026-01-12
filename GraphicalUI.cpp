5ab33b06e5597cda64c92a7eb9be021f62925614
Mohamed Badis Kerdellou <mohamedbadis.kerdellou@stud.h-da.de>
2026-01-12 10:40:20 +0100

    Corrected move() return type (int to void)

--- GraphicalUI.cpp
+++ GraphicalUI.cpp
@@ -1,24 +1,27 @@
 #include "GraphicalUI.h"
-#include "startscreen.h"
-#include "MainWindow.h"
-#include "DungeonCrawler.h"
-#include <sstream>
-#include <QPainter>
-#include <QMediaPlayer>
 #include <QAudioOutput>
-GraphicalUI::GraphicalUI(Level *lvl, DungeonCrawler *d) {
+#include <QMediaPlayer>
+#include <QPainter>
+#include "DungeonCrawler.h"
+#include "MainWindow.h"
+#include "startscreen.h"
+#include <sstream>
+GraphicalUI::GraphicalUI(Level *lvl, DungeonCrawler *d)
+{
     level = lvl;
     dc = d;
     startScreen = new StartScreen(this);
     mainWindow = new MainWindow(lvl, this);
 }
 
-GraphicalUI::~GraphicalUI() {
+GraphicalUI::~GraphicalUI()
+{
     delete startScreen;
     delete mainWindow;
 }
 
-void GraphicalUI::switchWindow() {
+void GraphicalUI::switchWindow()
+{
     if (currentWindow == 0) {
         startScreen->hide();
         mainWindow->show();
@@ -33,84 +36,85 @@ void GraphicalUI::switchWindow() {
 
 void GraphicalUI::playSound(QString soundLink, float volume)
 {
-    QMediaPlayer* player = new QMediaPlayer;
-    QAudioOutput* audioOutput = new QAudioOutput;
+    QMediaPlayer *player = new QMediaPlayer;
+    QAudioOutput *audioOutput = new QAudioOutput;
     player->setAudioOutput(audioOutput);
     player->setSource(QUrl(soundLink));
     audioOutput->setVolume(volume);
     player->play();
-
 }
 
-void GraphicalUI::draw(Level *level) {
-
+void GraphicalUI::draw(Level *level)
+{
     removeHealthBars();
     QGridLayout *gameBoard = mainWindow->getGameBoard();
     std::vector<std::vector<Tile *> > *tiles = level->getTiles();
-    for (int rowIterator=0; rowIterator<level->getHeight(); rowIterator++){
-        for (int colIterator=0;colIterator<level->getWidth();colIterator++){
-            Tile* currentTile = (*tiles)[rowIterator][colIterator];
-            QLayoutItem* itemAtTilePosition = gameBoard->itemAtPosition(rowIterator, colIterator);
-            if (!itemAtTilePosition){
+    for (int rowIterator = 0; rowIterator < level->getHeight(); rowIterator++) {
+        for (int colIterator = 0; colIterator < level->getWidth(); colIterator++) {
+            Tile *currentTile = (*tiles)[rowIterator][colIterator];
+            QLayoutItem *itemAtTilePosition = gameBoard->itemAtPosition(rowIterator, colIterator);
+            if (!itemAtTilePosition) {
                 QWidget *tileWidget = new QWidget(nullptr);
                 tileWidget->setMinimumSize(50, 50);
-                tileWidget->setFixedSize(50,50);
+                tileWidget->setFixedSize(50, 50);
                 std::ostringstream style;
                 std::ostringstream bStyle;
-                style << "border-image : url(" << currentTile->getTexturePath() << ") 0 0 0 0 stretch stretch;";
-                bStyle << "background-image:url(" << currentTile->getTexturePath() << "); background-repeat: no-repeat; background-position:center;";
+                style << "border-image : url(" << currentTile->getTexturePath()
+                      << ") 0 0 0 0 stretch stretch;";
+                bStyle << "background-image:url(" << currentTile->getTexturePath()
+                       << "); background-repeat: no-repeat; background-position:center;";
 
                 QString bgStyleFull = QString::fromStdString(style.str());
                 tileWidget->setStyleSheet(bgStyleFull);
 
                 gameBoard->addWidget(tileWidget, rowIterator, colIterator);
-            }
-            else{
-                QList<QWidget*> children = itemAtTilePosition->widget()->findChildren<QWidget*>(QString(),Qt::FindDirectChildrenOnly);
-                for (auto it= children.begin();it!=children.end();it++){
+            } else {
+                QList<QWidget *> children = itemAtTilePosition->widget()->findChildren<QWidget *>(
+                    QString(), Qt::FindDirectChildrenOnly);
+                for (auto it = children.begin(); it != children.end(); it++) {
                     delete (*it);
                 }
             }
 
-            if (currentTile->hasCharacter()){
-                QWidget* parentWidget = gameBoard->itemAtPosition(rowIterator, colIterator)->widget();
-                Character* characterAtTile = currentTile->getCharacter();
-                QWidget* characterWidget = new QWidget(parentWidget);
-                characterWidget->setFixedSize(50,40);
-                characterWidget->move(0,10);
+            if (currentTile->hasCharacter()) {
+                QWidget *parentWidget = gameBoard->itemAtPosition(rowIterator, colIterator)->widget();
+                Character *characterAtTile = currentTile->getCharacter();
+                QWidget *characterWidget = new QWidget(parentWidget);
+                characterWidget->setFixedSize(50, 40);
+                characterWidget->move(0, 10);
                 std::ostringstream style;
                 std::ostringstream bStyle;
-                style << "border-image : url(:" << characterAtTile->getTexturePath() <<
-                    ") 20 0 0 0 stretch stretch;";
+                style << "border-image : url(:" << characterAtTile->getTexturePath()
+                      << ") 20 0 0 0 stretch stretch;";
 
-                float healthPercentage = (static_cast<float>(currentTile->getCharacter()->getCurrentHP())/static_cast<float>(currentTile->getCharacter()->getMaxHP()))*100;
-                QWidget* healthBar = generateHealthBar(healthPercentage, nullptr);
-                healthBar->setFixedSize(50,10);
+                float healthPercentage
+                    = (static_cast<float>(currentTile->getCharacter()->getCurrentHP())
+                       / static_cast<float>(currentTile->getCharacter()->getMaxHP()))
+                      * 100;
+                QWidget *healthBar = generateHealthBar(healthPercentage, nullptr);
+                healthBar->setFixedSize(50, 10);
                 characterWidget->show();
                 // healthBar->show();
                 QString bgStyleFull = QString::fromStdString(style.str());
                 characterWidget->setStyleSheet(bgStyleFull);
                 addHealthBar(healthBar);
                 gameBoard->addWidget(healthBar, rowIterator, colIterator, Qt::AlignTop);
-
             }
-
         }
     }
-
 }
 
-QWidget* GraphicalUI::generateHealthBar(int percentage, QWidget* parent)
+QWidget *GraphicalUI::generateHealthBar(int percentage, QWidget *parent)
 {
-    QWidget* healthBar = new QWidget(parent);
+    QWidget *healthBar = new QWidget(parent);
     float stopRedAt = std::clamp(percentage / 100.0f, 0.0f, 1.0f);
-    if (stopRedAt<0){
-        stopRedAt=0;
-        percentage=0;
+    if (stopRedAt < 0) {
+        stopRedAt = 0;
+        percentage = 0;
     }
-    float startWhiteFrom = stopRedAt+0.01;
-    if (stopRedAt>=1){
-        stopRedAt=0.99;
+    float startWhiteFrom = stopRedAt + 0.01;
+    if (stopRedAt >= 1) {
+        stopRedAt = 0.99;
         startWhiteFrom = 1;
     }
     std::ostringstream style;
@@ -130,75 +134,76 @@ std::pair<int, int> GraphicalUI::move()
     return lastMove;
 }
 
-
-
-int GraphicalUI::move(std::pair<int, int> xymove) {
+void GraphicalUI::move(std::pair<int, int> xymove)
+{
     lastMove = xymove;
     dc->move();
 }
 
-std::pair<int, int> GraphicalUI::translateMove(int step) {
+std::pair<int, int> GraphicalUI::translateMove(int step)
+{
     std::pair<int, int> xymove;
     switch (step) {
-        /* to go down, you add +1 to the row, to go up -1 the row, right +1 the column, left -1 the column*/
-        case 1:
-            xymove = {1, -1};
-            return xymove;
-        case 2:
-            xymove = {1, 0};
-            return xymove;
-        case 3:
-            xymove = {1, 1}; /*1-3, 2,2*/
-            return xymove;
-        case 4:
-            xymove = {0, -1};
-            return xymove;
-        case 5:
-            xymove = {0, 0};
-            return xymove;
-        case 6:
-            xymove = {0, 1};
-            return xymove;
-        case 7:
-            xymove = {-1, -1};
-            return xymove;
-        case 8:
-            xymove = {-1, 0};
-            return xymove;
-        case 9:
-            xymove = {-1, 1};
-            return xymove;
+    /* to go down, you add +1 to the row, to go up -1 the row, right +1 the column, left -1 the column*/
+    case 1:
+        xymove = {1, -1};
+        return xymove;
+    case 2:
+        xymove = {1, 0};
+        return xymove;
+    case 3:
+        xymove = {1, 1}; /*1-3, 2,2*/
+        return xymove;
+    case 4:
+        xymove = {0, -1};
+        return xymove;
+    case 5:
+        xymove = {0, 0};
+        return xymove;
+    case 6:
+        xymove = {0, 1};
+        return xymove;
+    case 7:
+        xymove = {-1, -1};
+        return xymove;
+    case 8:
+        xymove = {-1, 0};
+        return xymove;
+    case 9:
+        xymove = {-1, 1};
+        return xymove;
     }
 }
 
-void GraphicalUI::start() {
+void GraphicalUI::start()
+{
     mainWindow->show();
 }
 
-std::pair<int, int> GraphicalUI::getLastMove() {
+std::pair<int, int> GraphicalUI::getLastMove()
+{
     return lastMove;
 }
 
-void GraphicalUI::addHealthBar(QWidget* healthBar)
+void GraphicalUI::addHealthBar(QWidget *healthBar)
 {
     m_healthBars.push_back(healthBar);
 }
 
 void GraphicalUI::removeHealthBars()
 {
-    for (auto it = m_healthBars.begin(); it != m_healthBars.end(); )
-    {
+    for (auto it = m_healthBars.begin(); it != m_healthBars.end();) {
         delete *it;
         it = m_healthBars.erase(it); // erase returns the next valid iterator
     }
 }
 
-
-
-QDialog *GraphicalUI::getStartScreen() {
+QDialog *GraphicalUI::getStartScreen()
+{
     return startScreen;
 }
 
-QMainWindow *GraphicalUI::getMainWindow() {
+QMainWindow *GraphicalUI::getMainWindow()
+{
     return mainWindow;
 }
--- GraphicalUI.h
+++ GraphicalUI.h
@@ -1,36 +1,36 @@
 #ifndef GRAPHICALUI_H
 #define GRAPHICALUI_H
-#include "AbstractUI.h"
 #include <QDialog>
 #include <QMainWindow>
-#include "MainWindow.h"
-#include "Level.h"
 #include <QPainter>
+#include "AbstractUI.h"
+#include "Level.h"
+#include "MainWindow.h"
 
 class DungeonCrawler;
 class GraphicalUI : public AbstractUI, public AbstractController
 {
 private:
-    QDialog* startScreen;
-    MainWindow* mainWindow;
-    int currentWindow {0};
-    Level* level;
-    DungeonCrawler* dc;
-    std::pair <int, int> lastMove;
-    std::vector <QWidget*> m_healthBars;
+    QDialog *startScreen;
+    MainWindow *mainWindow;
+    int currentWindow{0};
+    Level *level;
+    DungeonCrawler *dc;
+    std::pair<int, int> lastMove;
+    std::vector<QWidget *> m_healthBars;
 
 public:
-    GraphicalUI(Level* lvl, DungeonCrawler* d);
-    QDialog* getStartScreen();
-    QMainWindow* getMainWindow();
-    void draw (Level*) override;
-    QWidget* generateHealthBar(int percentage, QWidget* parent);
-    std::pair<int,int> move() override;
-    int move(std::pair<int,int> xymove);
-    std::pair<int,int> translateMove(int step) override;
+    GraphicalUI(Level *lvl, DungeonCrawler *d);
+    QDialog *getStartScreen();
+    QMainWindow *getMainWindow();
+    void draw(Level *) override;
+    QWidget *generateHealthBar(int percentage, QWidget *parent);
+    std::pair<int, int> move() override;
+    void move(std::pair<int, int> xymove);
+    std::pair<int, int> translateMove(int step) override;
     void start();
-    std::pair <int, int> getLastMove();
-    void addHealthBar(QWidget* healthBar);
+    std::pair<int, int> getLastMove();
+    void addHealthBar(QWidget *healthBar);
     void removeHealthBars();
     ~GraphicalUI();
     void playSound(QString soundLink, float volume);
