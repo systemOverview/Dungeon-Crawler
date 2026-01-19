5ade22942aeaef80faab4b986f64f5326e4d3a88
Mohamed Badis Kerdellou <mohamedbadis.kerdellou@stud.h-da.de>
2026-01-19 10:44:35 +0100

    added CharacterHealthChangeEvent functions definitions.

--- Event.cpp
+++ Event.cpp
@@ -19,10 +19,39 @@ std::vector<AnimateTileEvent::Visualization> AnimateTileEvent::getVisualizations
 //Start of CharacterHealthChangeEventEvent definitions.
 CharacterHealthChangeEvent::CharacterHealthChangeEvent(Character *character): m_character{character}{};
 Character* CharacterHealthChangeEvent::getCharacter(){return m_character;}
+
+bool CharacterHealthChangeEvent::isListenerSubscribedToThisEvent(EventListener* eventListener, CharacterHealthChangeEvent* event)
+{
+    assert(characterPreferenceRegister.count(eventListener)>0 && "isListenerSubscribedToThisEvent was supplied with an eventListener with no preferences");
+    for (auto subscribedCharactersiterator = characterPreferenceRegister[eventListener].begin(); subscribedCharactersiterator!=characterPreferenceRegister[eventListener].end(); subscribedCharactersiterator++){
+        if ((*subscribedCharactersiterator)==event->getCharacter()){
+            return true;
+        }
+    }
+    return false;
+}
+
 void CharacterHealthChangeEvent::registerListener(EventListener *eventListener, Character *characterToListenTo)
 {
+    qDebug() << eventListener;
+    assert(characterToListenTo!=nullptr && "Character supplied is nullptr");
     characterPreferenceRegister[eventListener].push_back(characterToListenTo);
     registerListener(eventListener);
+}
+
+void CharacterHealthChangeEvent::registerListener(EventListener *eventListener, std::vector<Character *> ListOfCharactersToListenTo)
+{
+    for (auto it = ListOfCharactersToListenTo.begin(); it!=ListOfCharactersToListenTo.end(); it++){
+        assert(*it!=nullptr && "Character supplied is nullptr");
+        characterPreferenceRegister[eventListener].push_back(*it);
+    }
+    registerListener(eventListener);
+
+}
+void CharacterHealthChangeEvent::notifyListeners(CharacterHealthChangeEvent* event){notifyListenersAccordingToRegister(event, &characterPreferenceRegister);}
+void CharacterHealthChangeEvent::notifySpecificListener(EventListener* eventListener, CharacterHealthChangeEvent *event){
+    qDebug() << eventListener;
+    eventListener->onCharacterHealthChange(event);
 }
 
 
@@ -35,7 +64,6 @@ bool TileChangeEvent::isListenerSubscribedToThisEvent(EventListener* eventListen
     assert(TilePreferenceRegister.count(eventListener)>0 && "isListenerSubscribedToThisEvent was supplied with an eventListener with no preferences");
     for (auto subscribedTilesIterator = TilePreferenceRegister[eventListener].begin(); subscribedTilesIterator!=TilePreferenceRegister[eventListener].end(); subscribedTilesIterator++){
         if ((*subscribedTilesIterator)==event->getChangedTile()){
-            (eventListener)->onTileChange(event);
             return true;
         }
     }
@@ -46,15 +74,15 @@ TileChangeEvent::ChangeType TileChangeEvent::getChangeType() const{return m_chan
 
 void TileChangeEvent::registerListener(EventListener* eventListener, Tile *TileToListenTo)
 {
-
+    assert(TileToListenTo!=nullptr && "Tile supplied is nullptr");
     TilePreferenceRegister[eventListener].push_back(TileToListenTo);
     registerListener(eventListener);
 }
 
 void TileChangeEvent::registerListener(EventListener* eventListener, std::vector<Tile *> ListOfTilesToListenTo)
 {
-
     for (auto it = ListOfTilesToListenTo.begin(); it!=ListOfTilesToListenTo.end(); it++){
+        assert(*it!=nullptr && "Tile supplied is nullptr");
         TilePreferenceRegister[eventListener].push_back(*it);
     }
     registerListener(eventListener);
