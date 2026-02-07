d3c7c246424525024df516f1918d5cc035b941d2
Mohamed Badis Kerdellou <mohamedbadis.kerdellou@stud.h-da.de>
2026-02-07 18:27:57 +0100

    Redesign of the start screen

--- startscreen.cpp
+++ startscreen.cpp
@@ -3,33 +3,43 @@
 #include <QPixmap>
 #include <QtWidgets>
 #include "ui_startscreen.h"
-#include <iostream>
-
-StartScreen::StartScreen(GraphicalUI *GUI, QWidget *parent)
+StartScreen::StartScreen(DungeonCrawler* DC, QWidget *parent)
     : QDialog(parent)
-    , ui(new Ui::StartScreen)
+    , ui(new Ui::StartScreen), m_dungeonCrawler{DC}
 {
-    graphicalUI = GUI;
-    ui->setupUi(this);
+    // graphicalUI = GUI;
+    // ui->setupUi(this);
+    QVBoxLayout* mainLayout = new QVBoxLayout(this);
+    QHBoxLayout* layout = new QHBoxLayout();
+    mainLayout->addStretch();
+    mainLayout->addLayout(layout);
+    QPushButton* startGame = new QPushButton("Start new game");
+    QPushButton* loadFile = new QPushButton("Load previous game");
+    this->setStyleSheet("QPushButton{background-color:red; color:white}");
+    layout->addWidget(startGame);
+    layout->addWidget(loadFile);
 
-    // this->setStyleSheet("background-image: :/pics/startscreen_dark.png;");
-    // QPixmap bkgnd(":/pics/startscreen_dark.png");
-    // bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
-    // QPalette palette;
-    // palette.setBrush(QPalette::Window, bkgnd);
-    // this->setPalette(palette);
-    connect(ui->newGameButton, &QPushButton::clicked, this, &StartScreen::switchWindow);
-    //     connect(ui->actionSearch, &QAction::triggered, this, &MainWindow::search);
+    connect(startGame, &QPushButton::clicked, [this]() {switchWindow(DungeonCrawler::FromStrings);});
+    connect(loadFile, &QPushButton::clicked, [this]() {switchWindow(DungeonCrawler::FromFile);});
+
+    setFixedSize(1000,600);
+
 }
-// QWidget::setStyleSheet("background-image: :/textures/switch.png;");
-//    redPixmap = new QPixmap(":/images/red.png");
 
 StartScreen::~StartScreen()
 {
     delete ui;
 }
 
-void StartScreen::switchWindow()
+void StartScreen::switchWindow(DungeonCrawler::GameSourceOption option)
 {
-    graphicalUI->switchWindow();
+    m_dungeonCrawler->buildGame(option);
+}
+
+void StartScreen::paintEvent(QPaintEvent *event)
+{
+    QPainter painter (this);
+    QPixmap background (":/pics/textures/start screen/startscreen_dark.png");
+    painter.drawPixmap(rect(), background);
+    QDialog::paintEvent(event);
 }
--- startscreen.h
+++ startscreen.h
@@ -2,6 +2,7 @@
 #define STARTSCREEN_H
 #include <QDialog>
 #include "GraphicalUI.h"
+#include "DungeonCrawler.h"
 class GraphicalUI;
 namespace Ui {
 class StartScreen;
@@ -12,13 +13,15 @@ class StartScreen : public QDialog
     Q_OBJECT
 
 public:
-    explicit StartScreen(GraphicalUI *GUI, QWidget *parent = nullptr);
+    explicit StartScreen(DungeonCrawler* DC, QWidget *parent = nullptr);
     ~StartScreen();
 
 private:
+    DungeonCrawler* m_dungeonCrawler = nullptr;
     Ui::StartScreen *ui;
     GraphicalUI *graphicalUI;
-    void switchWindow();
+    void switchWindow(DungeonCrawler::GameSourceOption option);
+    void paintEvent(QPaintEvent* event) override;
 };
 
 #endif // STARTSCREEN_H
