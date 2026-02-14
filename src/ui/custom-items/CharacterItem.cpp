4fae415bc096dc8da2178ea1f7f6d8a7fb10bfdc
Mohamed Badis Kerdellou <mohamedbadis.kerdellou@stud.h-da.de>
2026-02-14 19:03:34 +0100

    StateMachine implementation

--- src/ui/custom-items/CharacterItem.cpp
+++ src/ui/custom-items/CharacterItem.cpp
@@ -1,51 +1,100 @@
 #include "CharacterItem.h"
+#include <QFile>
+#include <QFinalState>
+#include <QSignalTransition>
+#include <QStateMachine>
+#include <QtCore/qpropertyanimation.h>
+#include <QtCore/qsequentialanimationgroup.h>
+#include <QtCore/qtimeline.h>
 #include <QtCore/qtimer.h>
 #include <QtGui/qbitmap.h>
 #include <QtGui/qpainter.h>
+#include <QtGui/qpicture.h>
 #include "SpriteManager.h"
+#include "animations/CharacterAnimation.h"
 CharacterItem::CharacterItem() {
-    m_timer = new QTimer();
-    m_timer->setTimerType(Qt::PreciseTimer);
-    QImage base = SpriteManager::GetImageFromSprite(CharacterGraphics::CharacterPart::Base);
-    m_parts.insert({CharacterGraphics::CharacterPart::Base, QPixmap::fromImage(base)});
-
-    QImage outfit = SpriteManager::GetImageFromSprite(CharacterGraphics::CharacterPart::Outfit);
-    m_parts.insert({CharacterGraphics::CharacterPart::Outfit, QPixmap::fromImage(outfit)});
-
-    QImage head = SpriteManager::GetImageFromSprite(CharacterGraphics::CharacterPart::Head);
-    m_parts.insert({CharacterGraphics::CharacterPart::Head, QPixmap::fromImage(head)});
+    setDefaultParts();
+    setupStateMachine();
 }
+
+void CharacterItem::setDefaultParts() {
+    for (int i = 0; i < int(CharacterGraphics::CharacterPart::PAST_END); i++) {
+        m_partsGraphicOptions.insert({CharacterGraphics::CharacterPart(i), 0});
+    }
+}
+
+void CharacterItem::startAnimationLoop() {
+    for (int i = 1; i < int(CharacterAnimation::AnimationType::PAST_END);
+         i++) { // 0 is idle (no animation)
+        CharacterAnimation* animation = new CharacterAnimation(this,
+                                                               CharacterAnimation::AnimationType(i));
+        m_customizationAnimationLoop->addAnimation(animation);
+    }
+
+    //I am unable to get animations to trigger automatically on state change for some reason
+    // So for now this loop repeats until the initial state is left (customizingState->playingState).
+    connect(m_customizationAnimationLoop, &QSequentialAnimationGroup::finished, [this]() {
+        if (m_stateMachine->configuration().contains(m_stateMachine->initialState())) {
+            m_customizationAnimationLoop->start();
+        }
+    });
+}
+
+void CharacterItem::setupAnimationMachine() {
+    m_animationMachine = new QStateMachine();
+    for (int i = 1; i < int(CharacterAnimation::AnimationType::PAST_END); i++) {
+        QState* state = new QState(m_animationMachine);
+        if (i == 1) m_animationMachine->setInitialState(state);
+        CharacterAnimation* animation = new CharacterAnimation(this,
+                                                               CharacterAnimation::AnimationType(i));
+        connect(state, &QState::entered, animation, [animation]() { animation->start(); });
+    }
+    m_animationMachine->start();
+};
 
 void CharacterItem::resize(qreal newTileLength) { m_size = newTileLength; }
 
-void CharacterItem::animateMove(QPointF newPos) {
+void CharacterItem::assignPart(CharacterGraphics::CharacterPart partType, int whichGraphicsOption) {
+    m_partsGraphicOptions.insert_or_assign(partType, whichGraphicsOption);
     update();
-    if (newPos == pos()) return;
-    if (m_lastStep == 1) {
-        m_startingPos = pos().x();
-    }
-    m_counter += 1;
-    setPos(pos().x() + 1, pos().y());
-    // QTimer::singleShot(animateMove(newPos))
-    if (m_counter == 9) {
-        m_counter = 1;
-    };
-    QTimer::singleShot(100, this, [this, newPos]() { animateMove(newPos); });
-    qDebug() << pos() << newPos;
 }
 
-void CharacterItem::assignPart(CharacterGraphics::CharacterPart partType, int partID) {
-    QPixmap partPixmap = QPixmap::fromImage(SpriteManager::GetImageFromSprite(partType, partID));
-    m_parts.insert_or_assign(partType, partPixmap);
-    update();
-}
+void CharacterItem::moveTo(QPointF newPos) {}
 
 QRectF CharacterItem::boundingRect() const { return {0, 0, m_size, m_size}; }
 
 void CharacterItem::paint(QPainter* painter,
                           const QStyleOptionGraphicsItem* option,
                           QWidget* widget) {
-    for (const auto& [wearableType, wearablePixMap] : m_parts) {
-        painter->drawPixmap(boundingRect(), wearablePixMap, wearablePixMap.rect());
+    for (const auto& [part, partOption] : m_partsGraphicOptions) {
+        QPixmap pixmap = SpriteManager::GetFrameFromSprite(part, partOption, m_currentFrameId);
+        painter->drawPixmap(boundingRect(), pixmap, pixmap.rect());
     }
+}
+
+void CharacterItem::setupStateMachine() {
+    m_stateMachine = new QStateMachine();
+    QState* customizingState = new QState(m_stateMachine);
+    m_customizationAnimationLoop = new QSequentialAnimationGroup;
+
+    QState* playingState = new QState(m_stateMachine);
+    QFinalState* deadState = new QFinalState(m_stateMachine);
+
+    connect(customizingState, &QState::entered, m_customizationAnimationLoop, [this]() {
+        m_customizationAnimationLoop->start();
+    });
+
+    connect(customizingState, &QState::finished, m_customizationAnimationLoop, [this]() {
+        m_customizationAnimationLoop->finished();
+    });
+
+    m_stateMachine->setInitialState(customizingState);
+    m_stateMachine->start();
+
+    startAnimationLoop();
+}
+
+void CharacterItem::setCurrentFrameId(int frameId) {
+    m_currentFrameId = frameId;
+    update();
 }
--- src/ui/custom-items/CharacterItem.h
+++ src/ui/custom-items/CharacterItem.h
@@ -3,25 +3,37 @@
 
 #include <QGraphicsItem>
 #include <QObject>
+#include <QtCore/qobject.h>
+#include <QtStateMachine/qstatemachine.h>
+#include "CharacterAnimation.h"
 #include "Constants.h"
 
 class CharacterItem : public QObject, public QGraphicsItem
 {
     Q_OBJECT
-    inline static int SECONDS_PER_MOVE = 2;
-    QTimer* m_timer;
-    QString m_texutreBase = ":/graphics/characters/sprite/0.png";
-    int m_counter = 1;
-    int m_lastStep = 1;
-    int m_startingPos = 0;
+    // The character only stores the current frame id (the same for every part)
+    // and the user-chosen graphics option for each part. When painting, it requests the images for each part from the
+    // sprite manager, which has a cache system in place to avoid re-generating images.
+    int m_currentFrameId = 0;
+    std::map<CharacterGraphics::CharacterPart, int> m_partsGraphicOptions = {};
     qreal m_size = 300;
-    int m_selectedArmor = 1;
-    std::map<CharacterGraphics::CharacterPart, QPixmap> m_parts = {};
+
+    QStateMachine* m_stateMachine = nullptr;
+    QStateMachine* m_animationMachine = nullptr;
+
+    QSequentialAnimationGroup* m_customizationAnimationLoop = nullptr;
+    void setDefaultParts();
+
+    void setupStateMachine();
+    void setupAnimationMachine();
+
+    void startAnimationLoop();
 
 public slots:
     void resize(qreal newTileLength);
-    void animateMove(QPointF newPos);
-    void assignPart(CharacterGraphics::CharacterPart partType, int partID);
+    void assignPart(CharacterGraphics::CharacterPart partType, int whichGraphicsOption);
+    void moveTo(QPointF newPos);
+signals:
 
 public:
     enum { Type = UserType + 2 };
@@ -29,6 +41,7 @@ public:
     CharacterItem();
     QRectF boundingRect() const override;
     void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
+    void setCurrentFrameId(int frameId);
 };
 
 #endif // CHARACTERITEM_H
