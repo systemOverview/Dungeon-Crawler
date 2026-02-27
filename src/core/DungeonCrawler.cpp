c209342ee11c99d9b84f19f82b45710e4542c031
Mohamed Badis Kerdellou <mohamedbadis.kerdellou@stud.h-da.de>
2026-02-27 09:05:55 +0100

    Refactor functions to be static whever possible

--- src/core/DungeonCrawler.cpp
+++ src/core/DungeonCrawler.cpp
@@ -8,8 +8,6 @@
 #include <QStandardPaths>
 #include <QWindow>
 #include <QtCore/qtimer.h>
-#include "startscreen.h"
-#include <fstream>
 
 // This class is follows the singleton pattern.
 // It communicates mainly with the GUI:
@@ -26,17 +24,16 @@
 
 DungeonCrawler::DungeonCrawler() {
     assert(SINGLETON_INSTANCE == nullptr && "Multiple instanced of DC are being created");
-    m_GUI = new GraphicalUI();
+    GUI = new GraphicalUI();
     SINGLETON_INSTANCE = this;
 
-    connect(m_GUI, &GraphicalUI::gameStarted, this, &DungeonCrawler::buildGame);
-    connect(m_GUI, &GraphicalUI::humanHasInitiatedMove, this, &DungeonCrawler::move);
+    connect(GUI, &GraphicalUI::gameStarted, this, &DungeonCrawler::buildGame);
+    connect(GUI, &GraphicalUI::humanHasInitiatedMove, this, &DungeonCrawler::move);
 }
 
 void DungeonCrawler::buildGame() {
-    Level* level = new Level(gameStrings[0]);
-    m_levels.push_back(level);
-    m_GUI->createLevelUI(level->getTiles());
+    Level* level = new Level(GameStrings[0]);
+    GUI->createLevelUI(level->getTiles());
 }
 
 void DungeonCrawler::ConnectGeneratedCharacter(Character* characterModel,
@@ -67,157 +64,4 @@ bool DungeonCrawler::IsTileInNeighbouringRange(Tile* from, Tile* to) {
 
 // End of ValidateMove helpers
 
-//old
 
-void DungeonCrawler::build(GameSourceOption option) {
-    if (option == GameSourceOption::FromStrings) {
-        for (std::string& gameString : gameStrings) {
-            Level* lvl = new Level(10, 10, gameString, true);
-            m_levels.push_back(lvl);
-            m_lastLevel = lvl;
-        }
-    }
-    else {
-        QString storagePath = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
-        std::string filePath = storagePath.toStdString() + "/level.json";
-        std::ifstream file(filePath);
-        json parsedJson = json::parse(file);
-        std::cout << parsedJson;
-        for (const auto& level : parsedJson["Levels"]) {
-            Level* lvl = new Level(level);
-            m_levels.push_back(lvl);
-            m_lastLevel = lvl;
-        }
-    }
-
-    m_startScreen->hide();
-
-    (*currentLevel)->activateLevel();
-}
-
-std::pair<int, int> DungeonCrawler::translateMove(int step) const {
-    std::pair<int, int> xymove;
-
-    switch (step) {
-    /* to go down, you add +1 to the row, to go up -1 the row, right +1 the column, left -1 the column*/
-    case 1:
-        xymove = {1, -1};
-        return xymove;
-    case 2:
-        xymove = {1, 0};
-        return xymove;
-    case 3:
-        xymove = {1, 1};
-        return xymove;
-    case 4:
-        xymove = {0, -1};
-        return xymove;
-    case 5:
-        xymove = {0, 0};
-        return xymove;
-    case 6:
-        xymove = {0, 1};
-        return xymove;
-    case 7:
-        xymove = {-1, -1};
-        return xymove;
-    case 8:
-        xymove = {-1, 0};
-        return xymove;
-    case 9:
-        xymove = {-1, 1};
-        return xymove;
-    }
-}
-
-bool DungeonCrawler::turn() {
-    m_GUI->getMainWindow()->show();
-    m_GUI->draw(*currentLevel);
-    return true;
-}
-
-void DungeonCrawler::levelUp() {
-    QTile::deleteAllQTiles();
-    delete (*currentLevel);
-    // currentLevel = m_levels.erase(currentLevel);
-    // TODO refacor this
-    (*currentLevel)->activateLevel();
-    m_GUI->draw(*currentLevel);
-    m_numberOfRemainingNPCs = (*currentLevel)->getNonPlayableCharacters().size();
-}
-
-void DungeonCrawler::moveCharacter(Character* character, Tile* tile, bool giveway) {
-    if (giveway) {
-        assert(character->getTexture() == 'A' && "A non attacker has asked to giveway");
-        if (tile->getCharacter() && tile->getCharacter()->getTexture() != 'P') {
-            Character* npc = tile->getCharacter();
-            Tile* attackerTile = character->getTile();
-
-            character->getTile()->setCharacter(nullptr);
-            character->setTile(tile);
-            tile->setCharacter(character);
-
-            npc->setTile(attackerTile);
-            attackerTile->setCharacter(npc);
-
-            return;
-        }
-    }
-    character->getTile()->setCharacter(nullptr);
-    character->setTile(tile);
-    tile->setCharacter(character);
-}
-
-Tile* DungeonCrawler::getEffectiveTile(Tile* currentTile, Tile* destinationTile) {
-    // Player can't move if onLeave is false(ie: pit->floor) or onEnter is false (ie: trying to enter a wall)
-    // Otherwise, if both are true, onEnter.first returns a pair: a bool indicating if a player might enter
-    // and a pointer to itself or to the effective destination tile (a portal would return its sibling).
-
-    if (currentTile->onLeave(destinationTile)) {
-        std::pair<bool, Tile*> onEnterResult = destinationTile->onEnter();
-        if (onEnterResult.first == false) {
-            return currentTile;
-        }
-        return onEnterResult.second;
-    }
-    return currentTile;
-}
-
-void DungeonCrawler::holdFight(Character* attacker, Character* defender, Tile* disputedTile) {
-    assert(attacker->isHuman() != defender->isHuman() && "Only Human VS NPC fights are allowed");
-    Character* killer = nullptr;
-    Character* victim = nullptr;
-    defender->decrementFromHP(attacker->getStrength());
-    if (defender->isAlive() == false) {
-        killer = attacker;
-        victim = defender;
-    }
-    else {
-        attacker->decrementFromHP(defender->getStrength());
-        if (attacker->isAlive() == false) {
-            killer = defender;
-            victim = attacker;
-        }
-    }
-
-    if (killer) {
-        m_GUI->playSound("qrc:/pics/textures/sounds/death.wav", 1);
-        victim->getTile()->setCharacter(nullptr);
-        moveCharacter(killer, disputedTile);
-        assert(victim != nullptr);
-        (*currentLevel)->removeCharacter(victim);
-        delete victim;
-        victim = nullptr;
-    }
-    else {
-        m_GUI->playSound("qrc:/pics/textures/sounds/punch.wav", 1);
-    }
-    if (killer && killer->getTexture() == 'P') {
-        m_numberOfRemainingNPCs -= 1;
-        if (m_numberOfRemainingNPCs <= 0 && (*currentLevel) == m_lastLevel) {
-            m_GUI->playSound("qrc:/pics/textures/sounds/win.ogg", 1);
-            ;
-            m_GUI->getMainWindow()->gameWon();
-        }
-    }
-}
--- src/core/DungeonCrawler.h
+++ src/core/DungeonCrawler.h
@@ -6,82 +6,46 @@
 #define PRAK_DUNGEONCRAWLER_H
 #include "GraphicalUI.h"
 #include "Level.h"
-#include "List.tpp"
-class StartScreen;
 class DungeonCrawler : public QObject
 {
     Q_OBJECT
 private:
     inline static DungeonCrawler* SINGLETON_INSTANCE = nullptr;
-    std::vector<Level*>::iterator currentLevel;
-    List<Level*> m_levels;
-    std::string m_gameString = "##########"
-                               "#........#"
-                               "#...<....#"
-                               "#..___...#"
-                               "#........#"
-                               "#........#"
-                               "#######.##"
-                               "#........#"
-                               "#........#"
-                               "##########";
 
-    std::array<std::string, 2> gameStrings = {"##########"
-                                              "#....$...#"
-                                              "#.S.<....#"
-                                              "#..___...#"
-                                              "#.G......#"
-                                              "#G.......#"
-                                              "#######.##"
-                                              "#.....A..#"
-                                              "#.P......#"
-                                              "##########",
-                                              "##########"
-                                              "#....!...#"
-                                              "#1A.<..S.#"
-                                              "#..___...#"
-                                              "#0.___...#"
-                                              "#........#"
-                                              "#######X##"
-                                              "#.G.?..1.#"
-                                              "#P.0...S.#"
-                                              "##########"};
-    GraphicalUI* m_GUI;
-    StartScreen* m_startScreen = nullptr;
-    int m_numberOfRemainingNPCs = 0;
-    Level* m_lastLevel = nullptr;
+    inline static std::array<std::string, 2> GameStrings = {"##########"
+                                                            "#....$...#"
+                                                            "#.S.<....#"
+                                                            "#..___...#"
+                                                            "#.G......#"
+                                                            "#G.......#"
+                                                            "#######.##"
+                                                            "#.....A..#"
+                                                            "#.P......#"
+                                                            "##########",
+                                                            "##########"
+                                                            "#....!...#"
+                                                            "#1A.<..S.#"
+                                                            "#..___...#"
+                                                            "#0.___...#"
+                                                            "#........#"
+                                                            "#######X##"
+                                                            "#.G.?..1.#"
+                                                            "#P.0...S.#"
+                                                            "##########"};
+    inline static GraphicalUI* GUI = nullptr;
+
+    // Move validation functions.
+    static bool ValidateMove(Tile* from, Tile* to);
     static bool IsTileInNeighbouringRange(Tile* from, Tile* to);
+
 public slots:
     void buildGame();
 signals:
     void move(); // alerts controllers to move their characters
 
 public:
-    enum GameSourceOption{
-        FromStrings,
-        FromFile
-    };
-
     DungeonCrawler();
     static void ConnectGeneratedCharacter(Character* characterModel, CharacterItem* characterView);
-    static bool ValidateMove(Tile* from, Tile* to);
-    //old:
-    std::pair<int, int> translateMove(int step) const;
-    bool turn();
-    void levelUp();
-    void moveCharacter(Character* character, Tile* tile, bool giveway = false);
-    //an attacker can ask the other NPCs to switch spots if he wants to access a tile controlled by them
-    // as the other NPCs don't know a path towards the character but move in predetermined paths
-
-    Tile* getEffectiveTile(Tile* currentTile, Tile* destinationTile);
-    // When moving to tile B, a player can actually land on tile C instead (portals).
-    // This function finds the actual tile the player would land on to be able to schedule resolution fights.
-
-    void holdFight(Character* attacker, Character* defender, Tile* disputedTile);
-    // attacker is the one that is trying to enter a tile already held by the defender.
-    // it deletes the loser and updates the tile and the winner, or keeps things as they were if it ended in a draw.
-    void build(GameSourceOption option);
-
 };
 
 #endif //PRAK_DUNGEONCRAWLER_H
