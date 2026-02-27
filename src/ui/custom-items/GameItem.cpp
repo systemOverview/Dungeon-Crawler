f63e37ecfcb3b89dc1e157ee81912b6f282a7848
Mohamed Badis Kerdellou <mohamedbadis.kerdellou@stud.h-da.de>
2026-02-27 13:02:29 +0100

    remove custom side length functionality

--- src/ui/custom-items/GameItem.cpp
+++ src/ui/custom-items/GameItem.cpp
@@ -3,24 +3,14 @@
 
 void GameItem::SetSideLength(qreal newSideLength) { SIDE_LENGTH = newSideLength; }
 
-GameItem::GameItem(QPixmap texture, qreal customSideLength)
-    : m_texture{texture}
-    , m_customSideLength{customSideLength} {}
+GameItem::GameItem(QPixmap texture)
+    : m_texture{texture} {}
 
-QRectF GameItem::boundingRect() const {
-    if (m_customSideLength > 0) {
-        return {0, 0, m_customSideLength, m_customSideLength};
-    }
-    return {0, 0, SIDE_LENGTH, SIDE_LENGTH};
-}
+QRectF GameItem::boundingRect() const { return {0, 0, SIDE_LENGTH, SIDE_LENGTH}; }
 
 void GameItem::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) {
     painter->drawPixmap(this->boundingRect(), m_texture, m_texture.rect());
 }
-
-int GameItem::getCustomSideLength() { return m_customSideLength; }
-
-void GameItem::setCustomSideLength(qreal newLength) { m_customSideLength = newLength; }
 
 void GameItem::resize() {
     fixMyPosition();
--- src/ui/custom-items/GameItem.h
+++ src/ui/custom-items/GameItem.h
@@ -9,24 +9,17 @@ class GameItem : public QObject, public QGraphicsItem
     // All game items have the same dimensions, painting is supposed to be uniform too,
     // MainWindow calculates the new dimension when the view is resized. calls the static function GameItem::setSideLength
     // emits dimensionsChanged (no args) which is connected to item->resize().
-    //
-    // fixMyPosition() is pure since its determined by the type of the item.
     Q_OBJECT
-    Q_PROPERTY(qreal customSideLength READ getCustomSideLength WRITE setCustomSideLength)
 
 protected:
     inline static qreal SIDE_LENGTH = 70;
-    qreal m_customSideLength
-        = -1; // if a texture has a custom length > 0, that length is used instead of the global one.
     QPixmap m_texture;
     virtual void fixMyPosition() = 0;
 
 public:
-    GameItem(QPixmap texture = QPixmap(), qreal customSideLength = -1);
+    GameItem(QPixmap texture = QPixmap());
     QRectF boundingRect() const override;
     void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
-    int getCustomSideLength();
-    void setCustomSideLength(qreal newLength);
 
 public slots:
     static void SetSideLength(qreal newSideLength);
