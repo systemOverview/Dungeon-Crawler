000deb52dd11a3283d494a89cf9d3b30dac9a779
Mohamed Badis Kerdellou <mohamedbadis.kerdellou@stud.h-da.de>
2026-03-03 12:35:29 +0100

    remove old code

--- src/core/Level.h
+++ src/core/Level.h
@@ -5,26 +5,16 @@
 #ifndef PRAK_LEVEL_H
 #define PRAK_LEVEL_H
 #include "tile.h"
-#include "LevelGraph.h"
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 class Character;
 class Tile;
 class Level
 {
-    inline static Character* HumanCharacter = Character::GenerateCharacter('P');
-    Tile* m_initialHumanCharacterTile = nullptr;
-    // Because the human player doesn't get deleted when level changes (to keep health data)
-    // humanCharacter is static, and when levels are initially constructed from a string, they save the tile where the characater is supposed to be
-    // and only when the level is activeted through activate() does the level place the character at that tile
     inline static std::vector<std::vector<Tile*>> TILES = {};
     std::vector<Character *> m_characters;
-    std::vector<Character *> nonPlayableCharacters;
-    std::pair <int,int> m_playingCharacterPosition;
     int m_gameHeight = 10;
     int m_gameWidth = 10;
-    LevelGraph* m_graph = new LevelGraph();
-    void addCharacter(Character* character);
 
 public: //static functions
     static Tile* GetTile(int row, int col);
