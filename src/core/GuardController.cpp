a8545b2d712afd387ea0bf4d7ac389167c6aee13
Mohamed Badis Kerdellou <mohamedbadis.kerdellou@stud.h-da.de>
2026-03-05 16:10:55 +0100

    removed old code

--- src/core/GuardController.cpp
+++
@@ -1,8 +0,0 @@
-#include "GuardController.h"
-#include "Constants.h"
-
-GuardController::GuardController(Character* controlledCharacter)
-    : AbstractController(controlledCharacter) {
-}
-
-Coordinates GuardController::getNextMove() { return {0, 0}; }
--- src/core/StationaryController.cpp
+++
@@ -1,7 +0,0 @@
-#include "StationaryController.h"
-#include "Constants.h"
-
-StationaryController::StationaryController(Character* controlledCharacter)
-    : AbstractController(controlledCharacter) {}
-
-Coordinates StationaryController::getNextMove() { return {0, 0}; }
