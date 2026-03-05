590909f6364cf38d705ae4cc94d378cfe5156823
Mohamed Badis Kerdellou <mohamedbadis.kerdellou@stud.h-da.de>
2026-03-05 16:12:53 +0100

    Create a common namespace for types to be shared by models and views

---
+++ src/constants/Types.h
@@ -0,0 +1,48 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <QtCore/qobject.h>
+#include <QtCore/qtmetamacros.h>
+
+// This namespace stores the types of items as they are shard by both models and views, to avoid having to include model inside view
+// or vice versa and creating coupling.
+namespace Types {
+    Q_NAMESPACE
+    enum class TileType {
+        Floor,
+        Wall,
+        Portal,
+        OpenDoor,
+        ClosedDoor,
+        Switch,
+        Pit,
+        Ramp,
+        LevelChanger,
+        GameWinner,
+        PAST_ENUM_END
+    };
+
+    enum class CharacterType { Human, StationaryZombie, GuardZombie, Attacker, PAST_ENUM_END };
+
+    Q_ENUM_NS(TileType);
+    Q_ENUM_NS(CharacterType);
+
+}; // namespace Types
+
+namespace TypesIdentifiers {
+    const inline static std::map<char, Types::TileType> CHAR_TO_TILE_TYPE_LOOKUP_TABLE{
+     {'.', Types::TileType::Floor},
+     {'#', Types::TileType::Wall},
+     {'&', Types::TileType::Portal},
+     {'X', Types::TileType::ClosedDoor},
+     {'/', Types::TileType::OpenDoor},
+     {'?', Types::TileType::Switch},
+     {'_', Types::TileType::Pit},
+     {'<', Types::TileType::Ramp},
+     {'$', Types::TileType::LevelChanger},
+     {'!', Types::TileType::GameWinner},
+    };
+
+}
+
+#endif // TYPES_H
